8cc87164f3c20531b9ec328c2a4aeda9eb6ed597
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-18 19:05:45 +0530

    Update network_stack_parameters.sh

--- parameters/network_stack_parameters.sh
+++ parameters/network_stack_parameters.sh
@@ -4,6 +4,6 @@
 STACK_ENV="dev"
 Environment="dev"
 ProjectName="saran"
-CLONE_TEMPLATE_BUCKET="my-clone-template-bucket"
+CLONE_TEMPLATE_BUCKET="test-cloudformation-template-clone-stack"
 
 NETWORK_STACK_PARAMETERS="ParameterKey=Environment,ParameterValue=${Environment} ParameterKey=ProjectName,ParameterValue=${ProjectName} ParameterKey=StackBucketName,ParameterValue=${CLONE_TEMPLATE_BUCKET}"
