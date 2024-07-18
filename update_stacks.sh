9238e97e31651b0ee735db144660636e145d3de4
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-18 15:19:05 +0530

    Update update_stacks.sh

--- update_stacks.sh
+++ update_stacks.sh
@@ -21,10 +21,37 @@ create_change_set() {
       --parameters ${parameters}
 
     aws cloudformation wait change-set-create-complete --stack-name "${full_stack_name}" --change-set-name "${change_set_name}"
-    aws cloudformation describe-change-set --stack-name "${full_stack_name}" --change-set-name "${change_set_name}"
+    
+    change_set_status=$(aws cloudformation describe-change-set --stack-name "${full_stack_name}" --change-set-name "${change_set_name}" --query 'Status' --output text)
+    echo "Change set status: ${change_set_status}"
+    
+    if [ "${change_set_status}" == "CREATE_COMPLETE" ]; then
+      notify_teams "${full_stack_name}" "${change_set_status}"
+    fi
+    
   else
     echo "Stack ${full_stack_name} does not exist."
   fi
+}
+
+# Function to send notifications to Microsoft Teams
+notify_teams() {
+  STACK_NAME=$1
+  STATUS=$2
+  STACK_URL="https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks?filteringText=&filteringStatus=active&viewNested=false"
+  
+  if [ "${STATUS}" == "CREATE_COMPLETE" ]; then
+    MESSAGE="Some new changes have occurred in ${STACK_NAME}. Please review and approve to execute."
+  elif [ "${STATUS}" == "FAILED" ]; then
+    MESSAGE="No changes detected in ${STACK_NAME}."
+  else
+    MESSAGE="Unknown status ${STATUS} for stack ${STACK_NAME}."
+  fi
+
+  WEBHOOK_URL="https://knackforge.webhook.office.com/webhookb2/93eea688-6368-4c47-8d54-92a7ba364b30@196eed21-c67a-4aae-a70b-9f97644d5d14/IncomingWebhook/a5fab871a77e4c3ab1f770a1ba50c18f/73c1d036-08b9-4dd3-8346-afa964097b0a"
+  PAYLOAD="{\"text\": \"${MESSAGE}<br>Status: ${STATUS}.<br>CloudFormation Stack URL: ${STACK_URL}\"}"
+  
+  curl -H "Content-Type: application/json" -d "${PAYLOAD}" "${WEBHOOK_URL}"
 }
 
 # Define stack parameters
