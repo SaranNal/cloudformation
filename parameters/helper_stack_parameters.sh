40e29230014ebf718a24587c98cbdd8d25deadc5
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-18 18:12:53 +0530

    Update helper_stack_parameters.sh

--- parameters/helper_stack_parameters.sh
+++ parameters/helper_stack_parameters.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # helper_stack_parameters.sh
 
-S3LogBucket="my-s3-log-bucket"
+S3LogBucket="saran-s3-access-logs"
 HELPER_STACK_PARAMETERS="ParameterKey=S3LogBucket,ParameterValue=${S3LogBucket}"
