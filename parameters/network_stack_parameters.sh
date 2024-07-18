440a80dd663a6e9e40116059a628432af564ce8f
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-18 18:42:29 +0530

    Update network_stack_parameters.sh

--- parameters/network_stack_parameters.sh
+++ parameters/network_stack_parameters.sh
@@ -6,4 +6,4 @@ Environment="dev"
 ProjectName="saran"
 CLONE_TEMPLATE_BUCKET="my-clone-template-bucket"
 
-NETWORK_STACK_PARAMETERS="ParameterKey=Environment,ParameterValue=${Environment} ParameterKey=STACK_ENV,ParameterValue=${STACK_ENV} ParameterKey=ProjectName,ParameterValue=${ProjectName} ParameterKey=StackBucketName,ParameterValue=${CLONE_TEMPLATE_BUCKET}"
+NETWORK_STACK_PARAMETERS="ParameterKey=Environment,ParameterValue=${Environment} ParameterKey=ProjectName,ParameterValue=${ProjectName} ParameterKey=StackBucketName,ParameterValue=${CLONE_TEMPLATE_BUCKET}"
