991d8ba8df1d93e8e364cdd3de70c194b5f0d4c5
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-19 11:23:59 +0530

    Update execute_change_sets.sh

--- scripts/execute_change_sets.sh
+++ scripts/execute_change_sets.sh
@@ -3,7 +3,7 @@
 STACK_ENV=$1
 
 check_and_execute_change_set() {
-  local stack_name=$1
+  stack_name=$1
 
   echo "Checking and executing change set for ${stack_name}"
   CHANGE_SET_STATUS=$(aws cloudformation describe-change-set --stack-name ${stack_name}-${STACK_ENV} --change-set-name ${stack_name}-changeset --query 'Status' --output text)
