960b4bbba1b4a47c4232f4875952e425559188ed
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-19 12:10:31 +0530

    Update and rename network_stack_parameters.sh to network_stack_parameters.json

--- parameters/network_stack_parameters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# network_stack_parameters.sh
-
-STACK_ENV="dev"
-Environment="dev"
-ProjectName="saran"
-CLONE_TEMPLATE_BUCKET="test-cloudformation-template-clone-stack"
-
-NETWORK_STACK_PARAMETERS="ParameterKey=Environment,ParameterValue=${Environment} ParameterKey=ProjectName,ParameterValue=${ProjectName} ParameterKey=StackBucketName,ParameterValue=${CLONE_TEMPLATE_BUCKET}"
