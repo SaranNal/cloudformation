87e385fbe3b8530510676adc47a550de8113d4fd
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-19 12:10:53 +0530

    Update infra_stack_parameters.sh

--- parameters/infra_stack_parameters.sh
+++ parameters/infra_stack_parameters.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-# infra_stack_parameters.sh
-
-SSLCertificateID="c038183c-5752-4c8f-b992-4f5c166c8b14"
-INFRA_STACK_PARAMETERS="ParameterKey=SSLCertificateID,ParameterValue=${SSLCertificateID}"
+{
+  "SSLCertificateID": "c038183c-5752-4c8f-b992-4f5c166c8b14",
+  "INFRA_STACK_PARAMETERS": [
+    {"ParameterKey": "SSLCertificateID", "ParameterValue": "c038183c-5752-4c8f-b992-4f5c166c8b14"}
+  ]
+}
