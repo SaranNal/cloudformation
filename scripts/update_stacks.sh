b56c31473f5864a01fbe421725faba62503f23dd
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-24 16:12:46 +0530

    Update update_stacks.sh

--- scripts/update_stacks.sh
+++ scripts/update_stacks.sh
@@ -33,16 +33,16 @@ load_parameters_from_json() {
   # Determine which type of parameter file it is and format accordingly
   case "${json_file}" in
     *common_parameters.json)
-      jq -r '.COMMON_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "${json_file}"
+      jq -r '.COMMON_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\"\(.ParameterValue)""' "${json_file}"
       ;;
     *helper_stack_parameters.json)
-      jq -r '.HELPER_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "${json_file}"
+      jq -r '.HELPER_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\"\(.ParameterValue)""' "${json_file}"
       ;;
     *infra_stack_parameters.json)
-      jq -r '.INFRA_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "${json_file}"
+      jq -r '.INFRA_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\"\(.ParameterValue)""' "${json_file}"
       ;;
     *network_stack_parameters.json)
-      jq -r '.NETWORK_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "${json_file}"
+      jq -r '.NETWORK_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\"\(.ParameterValue)""' "${json_file}"
       ;;
     *)
       echo "Error: JSON file ${json_file} has an unknown format."
