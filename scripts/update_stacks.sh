b33f8ec3de03923d863e867c72ea5d7ebeb96e7b
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-24 16:53:21 +0530

    Update update_stacks.sh

--- scripts/update_stacks.sh
+++ scripts/update_stacks.sh
@@ -39,7 +39,7 @@ load_parameters_from_json() {
       jq -r '.HELPER_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "${json_file}"
       ;;
     *infra_stack_parameters.json)
-      jq -r '.INFRA_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(if type=="array" then .ParameterValue | join(",") else .ParameterValue end)"' "${json_file}"
+      jq -r '.INFRA_STACK_PARAMETERS[] | if (.ParameterValue | type) == "array" then "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue | join(","))" else "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)" end' "${json_file}"
       ;;
     *network_stack_parameters.json)
       jq -r '.NETWORK_STACK_PARAMETERS[] | "ParameterKey=\(.ParameterKey),ParameterValue=\(.ParameterValue)"' "${json_file}"
