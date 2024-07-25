a66ac965f0622c9d27aeb51706bf569f5d272aaa
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-25 10:18:36 +0530

    Update update_stacks.sh

--- scripts/update_stacks.sh
+++ scripts/update_stacks.sh
@@ -117,7 +117,7 @@ send_notification() {
 declare -A stacks
 stacks[helper]="https://test-cloudformation-template-current-stack.s3.amazonaws.com/helper-stack/RootStack.yaml ./parameters/common_parameters.json ./parameters/helper_stack_parameters.json"
 stacks[network]="https://test-cloudformation-template-current-stack.s3.amazonaws.com/network-stack/RootStack.yaml ./parameters/network_stack_parameters.json"
-stacks[infra]="https://test-cloudformation-template-current-stack.s3.amazonaws.com/infra-stack/RootStack.yaml ./parameters/common_parameters.json ./parameters/infra_stack_parameters.json"
+stacks[infra]="https://test-cloudformation-template-current-stack.s3.amazonaws.com/infra-stack/RootStack.yaml ./parameters/common_parameters.json ./parameters/infra_stack_parameters.json ./parameters/infra_stack_parameters.sh"
 
 # Loop through each stack and create/update the change set
 for stack_name in "${!stacks[@]}"; do
