efef618a17ab7721f089247c20faaa90de7ca520
SaranNal <153280413+SaranNal@users.noreply.github.com>
2024-07-26 14:59:29 +0530

    Update update_stacks.sh

--- scripts/update_stacks.sh
+++ scripts/update_stacks.sh
@@ -41,20 +41,45 @@ update_stack() {
 }
 
 # Function to accumulate messages to be sent to Microsoft Teams
+# accumulate_message() {
+#   STACK_NAME=$1
+#   STATUS=$2
+#   STACK_URL="https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks?filteringText=&filteringStatus=active&viewNested=false"
+  
+#   if [ "${STATUS}" == "CREATE_COMPLETE" ]; then
+#     MESSAGE="Some new changes have occurred in **${STACK_NAME}**. Please review and approve to execute."
+#   elif [ "${STATUS}" == "FAILED" ]; then
+#     MESSAGE="No changes detected in **${STACK_NAME}**."
+#   else
+#     MESSAGE="Unknown status ${STATUS} for stack **${STACK_NAME}**."
+#   fi
+
+#   NOTIFICATION_MESSAGES+="${MESSAGE}<br>Status: **${STATUS}**.<br>CloudFormation Stack URL: ${STACK_URL}<br><br>"
+# }
+
+# # Function to send the accumulated notification to Microsoft Teams
+# send_notification() {
+#   WEBHOOK_URL="https://knackforge.webhook.office.com/webhookb2/93eea688-6368-4c47-8d54-92a7ba364b30@196eed21-c67a-4aae-a70b-9f97644d5d14/IncomingWebhook/a5fab871a77e4c3ab1f770a1ba50c18f/73c1d036-08b9-4dd3-8346-afa964097b0a"
+#   PAYLOAD="{\"text\": \"${NOTIFICATION_MESSAGES}\"}"
+  
+#   curl -H "Content-Type: application/json" -d "${PAYLOAD}" "${WEBHOOK_URL}"
+# }
 accumulate_message() {
   STACK_NAME=$1
   STATUS=$2
   STACK_URL="https://us-east-1.console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks?filteringText=&filteringStatus=active&viewNested=false"
   
-  if [ "${STATUS}" == "CREATE_COMPLETE" ]; then
-    MESSAGE="Some new changes have occurred in **${STACK_NAME}**. Please review and approve to execute."
-  elif [ "${STATUS}" == "FAILED" ]; then
-    MESSAGE="No changes detected in **${STACK_NAME}**."
-  else
-    MESSAGE="Unknown status ${STATUS} for stack **${STACK_NAME}**."
+  if [ -z "${NOTIFICATION_MESSAGES}" ]; then
+    NOTIFICATION_MESSAGES="**New updates for the CloudFormation templates**:<br><br>"
   fi
 
-  NOTIFICATION_MESSAGES+="${MESSAGE}<br>Status: **${STATUS}**.<br>CloudFormation Stack URL: ${STACK_URL}<br><br>"
+  if [ "${STATUS}" == "CREATE_COMPLETE" ]; then
+    MESSAGE="Some new changes have occurred in **${STACK_NAME}**. Please review and approve to execute."
+    NOTIFICATION_MESSAGES+="${MESSAGE}<br>Status: **${STATUS}**.<br>CloudFormation Stack URL: ${STACK_URL}<br><br>"
+  elif [ "${STATUS}" != "FAILED" ]; then
+    MESSAGE="Unknown status ${STATUS} for stack **${STACK_NAME}**."
+    NOTIFICATION_MESSAGES+="${MESSAGE}<br>Status: **${STATUS}**.<br>CloudFormation Stack URL: ${STACK_URL}<br><br>"
+  fi
 }
 
 # Function to send the accumulated notification to Microsoft Teams
